5c7c7308a904306ac296fa34ddb61adaeb172636
manojdsgowda <manojdsgowda@gmail.com>
2019-12-02 17:51:48 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,16 @@ export class AppComponent  {
         {field: 'EmployeeCode', editable: true },
         {field: 'EmployeeName', editable: true},
         {field: 'DOJ', editable: true},
-        {field: 'Gender', editable: true}
+        {field: 'Gender', editable: true},
+        {field: 'MaritalStatus', editable: true},
+        {field: 'ContactNo', editable: true}
     ];
 
     rowData = [
-        { EmployeeID: '1', EmployeeCode: 'MDJ', EmployeeName: 'Manoj D S', DOJ:'12-Dec-2019', Gender:'Male' },
-        { EmployeeID: '2', EmployeeCode: 'VIC', EmployeeName: 'Vinod R Chougule', DOJ:'05-Sep-2019', Gender:'Male' },
-        { EmployeeID: '3', EmployeeCode: 'SPU', EmployeeName: 'Sridhar Hanumanthappa', DOJ:'20-Sep-2019', Gender:'Male' }
+        { EmployeeID: '1', EmployeeCode: 'MDJ', EmployeeName: 'Manoj D S', DOJ:'12-Dec-2019', Gender:'Male',
+        MaritalStatus:'Single', ContactNo:'1234567890' },
+        { EmployeeID: '2', EmployeeCode: 'VIC', EmployeeName: 'Vinod R Chougule', DOJ:'05-Sep-2019', Gender:'Male', MaritalStatus:'Married', ContactNo:'876543211' },
+        { EmployeeID: '3', EmployeeCode: 'SPU', EmployeeName: 'Sridhar Hanumanthappa', DOJ:'20-Sep-2019', Gender:'Male', MaritalStatus:'Married', ContactNo:'234567890' }
     ];
 
     modules = AllCommunityModules;
